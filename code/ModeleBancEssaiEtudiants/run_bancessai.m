2c2b9db9fa92c7aaff3d59c792c627fae11ff810
Eric Matte <Eric Matte>
2016-06-28 15:31:59 -0400

    Déplacement de /Autres/MatlabProjet à /ModeleBancEssaiEtudiants/bancessai_ini.

--- code/ModeleBancEssaiEtudiants/run_bancessai.m
+++ code/ModeleBancEssaiEtudiants/run_bancessai.m
@@ -13,7 +13,7 @@ Ayeq = 0;               %en degres
 Pzeq = .015;            %en metres
 
 %Exemple de trajectoire
-t_des     = [0:1:8]'*5;
+t_des     = (0:1:8)'*5;
 x_des     = [t_des, [0 0 0.5 1  0 -1 0 1 0]'*0.05];
 y_des     = [t_des, [0 0 0 0 -1  0 1 0 0]'*0.05];
 z_des     = [t_des, [1 1 1 1  1  1 1 1 1]'*.015];
@@ -21,8 +21,8 @@ tfin = 50;
 
 %initialisation
 bancEssaiConstantes
-%bancessai_ini  %faites tous vos calculs de modele ici
-MatlabProjet
+bancessai_ini  %faites tous vos calculs de modele ici
+
 %Calcul des compensateurs
 %iniCTL_ver4    %Calculez vos compensateurs ici
 
