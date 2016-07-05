a1da956bf82c32373090b9d3c638c9c29483e25e
llepitre <louis.lepitre@usherbrooke.ca>
2016-07-05 11:34:33 -0400

    Add files via upload

--- code/ModeleBancEssaiEtudiants/comp_actionneur.m
+++ code/ModeleBancEssaiEtudiants/comp_actionneur.m
@@ -3,8 +3,10 @@ equilibre
 
 % Comparaison modèle des actionneurs --------------------------------------
 % *Fait avec seulement un déplacement en hauteur et non en angle
-for Z = 0:0.0001:0.03
-    Z0 = Z;
+
+for Z0 = 0:0.001:0.03
+   
+    Z = Z0;
     dZ = Z - Z0_eq;
     syms Ia Ib Ic
     [Ia_eq2, Ib_eq2, Ic_eq2] = solve([subs(d2_phi) == 0, subs(d2_theta) == 0, subs(d2_Z) == 0], [Ia, Ib, Ic]);
@@ -14,17 +16,21 @@ for Z = 0:0.0001:0.03
     dtheta = 0;
     XX = XB;
     YX = YB;
-    FX = subs(FX);
+    Fk = subs(FX);
     % On veut évaluer dFX à l'équilibre
     Z = Z0_eq;
     Ix = Ib_eq;
-    dFX = subs(dFX);
-    err_lin = 100*abs(dFX/FX); % en %
+    dFk = subs(dFX);
+    err_lin = 100*abs(dFk/Fk); % en %
     
     % Graphique de l'erreur de l'approcimation par rapport à Z
     figure(1)
-    plot(Z0, err_lin)
+    plot(Z0, err_lin, '*')
+    title(['Erreur abs de la version linéaire du modèle des actionneurs avec Z_{eq} = ',num2str(Z0_eq), 'm'])
+    xlabel('Postion de la plaque en Z (m)')
+    ylabel('Erreur abs lin vs non-lin (%)')
     hold on
+    grid on
 end
 
 % Pour avoir err_lin < 10% : si Z_eq = 0, dZ_max = +0.0002
