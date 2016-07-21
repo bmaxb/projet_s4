b44f4a049c4bbd1a0312d39b6bf121422cb6fe3b
Eric Matte <Eric Matte>
2016-07-21 16:33:19 -0400

    code

--- code/RFID/RFID.m
+++ code/RFID/RFID.m
@@ -8,10 +8,11 @@ clc; clear; close all
 % 4 FH2
 % 5 FL3
 % 6 FH3
-M = 2;
+M = 3;
 F0 = 436327400; % 436.3274 MHz
 Bw = 330000; % 330 kHz
 load('signaux.mat')
+
 
 % Calcul de F_H et F_L en fonction de M -----------------------------------
 % +/- 2.2 kHz
@@ -21,8 +22,19 @@ for i = 1:M
 end
 F_L = F_H - Bw/2;
 
+% Oscillateur local L_O2 --------------------------------------------------
+L_O2 = 10600000; % Hz
+N = length(signal_1a);
+dx = 0:0.001:2;
+oscill = @(x) sin(2*pi*L_O2*x);
+plot(dx, oscill(dx))
+figure
+
 % Demodulateur AM ---------------------------------------------------------
 dem = abs(signal_1a);
-plot(time, signal_1a)
+plot(signal_1a)
+figure
+
+plot(abs(fft(signal_1a(1400:3200))))
 % baud_a1 = liste des symboles transmis de signal_1a
 
