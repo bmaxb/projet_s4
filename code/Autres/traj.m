252ad3acbccb3eadb3226d0370c8d9beac78f970
AdamL8 <AdamL8@users.noreply.github.com>
2016-08-02 12:18:29 -0400

    Update traj.m

--- code/Autres/traj.m
+++ code/Autres/traj.m
@@ -8,4 +8,4 @@ Ni = [0 5; 2 8; 3 10; 4 13; 5 17];
 v_ab = 5;
 Ts = 0.01;
 
-[Pi Ltr E Vr Traj tt Tab_banc_essai] = trajectoire(Ni, v_ab, Ts);
+[Pi Ltr E Vr Traj tt Tab_banc_essai] = trajectoireCorrigé(Ni, v_ab, Ts);
